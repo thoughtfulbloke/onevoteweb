3f0999fec659423ca773a7e52997b3f4cafc1427
thoughtfulboke <david@thoughtful.net.nz>
2014-06-02 21:12:49 +1200

    custom webpage

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ simulated_election <- function(polldata){
     comment = "",
     text = "
 
-Party           Side  Electorate  Support
+Party           Allies  Electorate  Support
 ACT             n        1              0
 Conservative    n        0              0
 Green           l        0              0
@@ -20,22 +20,40 @@ Labour          l       22              0
 Mana            l        1              0
 Maori           n        3              0
 National        n       42              0
-NZ_First        w        0              0
+NZ_First        u        0              0
 United_Future   n        1              0
-Internet        l        0              0
 ")
   
-  sample_size <- polldata$sample[1]
-  party["National","Support"] <- polldata$national[1]
-  party["Labour","Support"] <- polldata$labour[1]
-  party["Green","Support"] <- polldata$green[1]
-  party["NZ_First","Support"] <- polldata$nzfirst[1]
-  party["Maori","Support"] <- polldata$maori[1]
-  party["Mana","Support"] <- polldata$mana[1]
-  party["ACT","Support"] <- polldata$act[1]
-  party["United_Future","Support"] <- polldata$united[1]
-  party["Conservative","Support"] <- polldata$conservative[1]
-  party["Internet","Support"] <- polldata$internet[1]
+sample_size <- polldata$sample[1]
+party["National","Support"] <- polldata$national_support[1]
+party["Labour","Support"] <- polldata$labour_support[1]
+party["Green","Support"] <- polldata$green_support[1]
+party["NZ_First","Support"] <- polldata$nzfirst_support[1]
+party["Maori","Support"] <- polldata$maori_support[1]
+party["Mana_Internet","Support"] <- polldata$manainternet_support[1]
+party["ACT","Support"] <- polldata$act_support[1]
+party["United_Future","Support"] <- polldata$united_support[1]
+party["Conservative","Support"] <- polldata$conservative_support[1]
+
+party["National","Electorate"] <- polldata$national_electorate[1]
+party["Labour","Electorate"] <- polldata$labour_electorate[1]
+party["Green","Electorate"] <- polldata$green_electorate[1]
+party["NZ_First","Electorate"] <- polldata$nzfirst_electorate[1]
+party["Maori","Electorate"] <- polldata$maori_electorate[1]
+party["Mana_Internet","Electorate"] <- polldata$manainternet_electorate[1]
+party["ACT","Electorate"] <- polldata$act_electorate[1]
+party["United_Future","Electorate"] <- polldata$united_electorate[1]
+party["Conservative","Electorate"] <- polldata$conservative_electorate[1]
+
+party["National","Allies"] <- polldata$national_allies[[1]]
+party["Labour","Allies"] <- polldata$labour_allies[1]
+party["Green","Allies"] <- polldata$green_allies[1]
+party["NZ_First","Allies"] <- polldata$nzfirst_allies[1]
+party["Maori","Allies"] <- polldata$maori_allies[1]
+party["Mana_Internet","Allies"] <- polldata$manainternet_allies[1]
+party["ACT","Allies"] <- polldata$act_allies[1]
+party["United_Future","Allies"] <- polldata$united_allies[1]
+party["Conservative","Allies"] <- polldata$conservative_allies[1]
   
   result = c("national_led", "labour_led", "hung", "nzf_decides")
   count = c(0,0,0,0)
@@ -72,10 +90,9 @@ Internet        l        0              0
     t <- tabulate(r[o[1:total_seats]])
     seats[1:length(t)] <- t
     seats <- (pmax(seats, party$Electorate))
-    
-    nseats <- sum(seats[party$Side == "n"])
-    lseats <- sum(seats[party$Side == "l"])
-    wseats <- sum(seats[party$Side == "w"])
+    nseats <- sum(seats[party$Allies == "n"])
+    lseats <- sum(seats[party$Allies == "l"])
+    wseats <- sum(seats[party$Allies == "u"])
     
     if (nseats > (lseats + wseats))  {
       outcomes["national_led","count"] <- outcomes["national_led","count"] + 1
@@ -91,15 +108,72 @@ Internet        l        0              0
   return(outcomes)
 }
 
+prepdata <- function(polldata){
+  party <- read.table(
+    header = TRUE,
+    row.names = "Party",
+    stringsAsFactors = FALSE,
+    fill = TRUE,
+    comment = "",
+    text = "
+
+Party           Colour   Allies  Electorate  Support
+ACT             #FFCB05  n        1              0
+Conservative    #00AEEF  n        0              0
+Green           #098137  l        0              0
+Labour          #FF0000  l       22              0
+Mana_Internet   #770808  l        1              0
+Maori           #EF4A42  n        3              0
+National        #00529F  n       42              0
+NZ_First        #000000  u        0              0
+United_Future   #501557  n        1              0
+")
+
+party["National","Support"] <- polldata$national_support[1]
+party["Labour","Support"] <- polldata$labour_support[1]
+party["Green","Support"] <- polldata$green_support[1]
+party["NZ_First","Support"] <- polldata$nzfirst_support[1]
+party["Maori","Support"] <- polldata$maori_support[1]
+party["Mana_Internet","Support"] <- polldata$manainternet_support[1]
+party["ACT","Support"] <- polldata$act_support[1]
+party["United_Future","Support"] <- polldata$united_support[1]
+party["Conservative","Support"] <- polldata$conservative_support[1]
+
+party["National","Electorate"] <- polldata$national_electorate[1]
+party["Labour","Electorate"] <- polldata$labour_electorate[1]
+party["Green","Electorate"] <- polldata$green_electorate[1]
+party["NZ_First","Electorate"] <- polldata$nzfirst_electorate[1]
+party["Maori","Electorate"] <- polldata$maori_electorate[1]
+party["Mana_Internet","Electorate"] <- polldata$manainternet_electorate[1]
+party["ACT","Electorate"] <- polldata$act_electorate[1]
+party["United_Future","Electorate"] <- polldata$united_electorate[1]
+party["Conservative","Electorate"] <- polldata$conservative_electorate[1]
+
+party["National","Allies"] <- polldata$national_allies[[1]]
+party["Labour","Allies"] <- polldata$labour_allies[1]
+party["Green","Allies"] <- polldata$green_allies[1]
+party["NZ_First","Allies"] <- polldata$nzfirst_allies[1]
+party["Maori","Allies"] <- polldata$maori_allies[1]
+party["Mana_Internet","Allies"] <- polldata$manainternet_allies[1]
+party["ACT","Allies"] <- polldata$act_allies[1]
+party["United_Future","Allies"] <- polldata$united_allies[1]
+party["Conservative","Allies"] <- polldata$conservative_allies[1]
+
+return(party)
+}
 
 shinyServer(
   function(input, output) {
-    output$distPlot <- renderPlot({
+    output$outplot <- renderPlot({
       outcomes <- simulated_election(input)
       freq <- 100 * outcomes$count / sum (outcomes$count)
       axis_text <- c( "National led\nw/o NZF", "Labour led\nw/o NZF", "Hung Parliament", "Up to NZF")
       colourscheme <- c("#0000FFFF","#FF0000FF","#FF00FFFF", "#000000FF")
-      barplot(freq, names.arg=axis_text, main="If the poll is accurate,\nthe election would be", cex.names=0.6, col=colourscheme, ylab="Results Percentage")
+      barplot(freq, names.arg=axis_text, main="If the poll is accurate,\nthe election would be", cex.names=0.9, col=colourscheme, ylab="Results Percentage")
+    })
+    output$inplot <- renderPlot({
+      parties <- prepdata(input)
+      barplot(parties$Support, names.arg=row.names(parties), main="Individual results", cex.names=0.9, col=parties$Colour, ylab="Support")
     })
   }
 )
